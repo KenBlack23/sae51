aab4e55e5b8c92897d04a394492ed089c496611a
Thierno Sow <thierno.ppc@gmail.com>
2024-09-06 16:16:43 +0200

    rajout du path VBoxmanage

--- genMV.bat
+++ genMV.bat
@@ -5,15 +5,15 @@ set RAM_SIZE=4096
 set HDD_SIZE=64000
 
 :: Création de la machine virtuelle
-VBoxManage createvm --name %VM_NAME% --ostype "Debian_64" --register
-VBoxManage modifyvm %VM_NAME% --memory %RAM_SIZE% --nic1 nat --boot1 net
-VBoxManage createmedium disk --filename "%VM_NAME%_disk.vdi" --size %HDD_SIZE%
-VBoxManage storagectl %VM_NAME% --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach %VM_NAME% --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "%VM_NAME%_disk.vdi"
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createvm --name %VM_NAME% --ostype "Debian_64" --register
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm %VM_NAME% --memory %RAM_SIZE% --nic1 nat --boot1 net
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createmedium disk --filename "%VM_NAME%_disk.vdi" --size %HDD_SIZE%
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl %VM_NAME% --name "SATA Controller" --add sata --controller IntelAHCI
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach %VM_NAME% --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "%VM_NAME%_disk.vdi"
 
 :: Pause pour vérifier la création
 pause
 
 :: Suppression de la machine virtuelle
-VBoxManage unregistervm %VM_NAME% --delete
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" unregistervm %VM_NAME% --delete
 
